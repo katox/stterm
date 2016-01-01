1728cc5aa15011df75d47a8332b16ae5deba7d2b
Kamil Toman <kamil.toman@leafclick.com>
2016-01-02 00:00:24 +0100

    Increase terminal history from 2000 to 5000 lines

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@
  */
 static char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
-#define histsize 2000
+#define histsize 5000
 
 /*
  * What program is execed by st depends of these precedence rules:
